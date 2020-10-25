f87eb0472f9d2ce225786ee74cbd8902317fa116
FFYzz <cryptochen95@gmail.com>
2020-10-25 09:40:34 +0800

    update generic part

---
+++ event/src/main/java/cn/ffyzz/spring/event/ObserverDemo.java
@@ -0,0 +1,7 @@
+package cn.ffyzz.spring.event;/**
+ * @Title: 
+ * @Author: FFYzz
+ * @Mail: cryptochen95 at gmail dot com
+ * @Date: 2020/10/25
+ */public class ObserverDemo {
+}
--- generic/src/main/java/cn/ffyzz/spring/generic/GenericCollectionTypeResolverDemo.java
+++ generic/src/main/java/cn/ffyzz/spring/generic/GenericCollectionTypeResolverDemo.java
@@ -1,6 +1,5 @@
 package cn.ffyzz.spring.generic;
 
-import org.springframework.core.GenericCollectionTypeResolver;
 import org.springframework.core.GenericTypeResolver;
 
 import java.lang.reflect.Field;
@@ -18,25 +17,25 @@ import java.util.ArrayList;
  */
 public class GenericCollectionTypeResolverDemo {
 
-    private static ArrayList<String> list;
-
-    private static StringList strings;
-
-    public static void main(String[] args) throws NoSuchFieldException {
-
-        // 返回泛型参数中的具体类型
-        System.out.println(GenericCollectionTypeResolver.getCollectionType(StringList.class));
-
-        // 没有指定具体类型，返回 null
-        System.out.println(GenericCollectionTypeResolver.getCollectionType(ArrayList.class));
-
-        Field field = GenericCollectionTypeResolverDemo.class.getDeclaredField("list");
-
-        System.out.println(GenericCollectionTypeResolver.getCollectionFieldType(field));
-
-        field = GenericCollectionTypeResolverDemo.class.getDeclaredField("strings");
-
-        System.out.println(GenericCollectionTypeResolver.getCollectionFieldType(field));
-    }
+//    private static ArrayList<String> list;
+//
+//    private static StringList strings;
+//
+//    public static void main(String[] args) throws NoSuchFieldException {
+//
+//        // 返回泛型参数中的具体类型
+//        System.out.println(GenericCollectionTypeResolver.getCollectionType(StringList.class));
+//
+//        // 没有指定具体类型，返回 null
+//        System.out.println(GenericCollectionTypeResolver.getCollectionType(ArrayList.class));
+//
+//        Field field = GenericCollectionTypeResolverDemo.class.getDeclaredField("list");
+//
+//        System.out.println(GenericCollectionTypeResolver.getCollectionFieldType(field));
+//
+//        field = GenericCollectionTypeResolverDemo.class.getDeclaredField("strings");
+//
+//        System.out.println(GenericCollectionTypeResolver.getCollectionFieldType(field));
+//    }
 
 }
