5bdda131b5421cf4de7f55b7ee72d0402ace55bc
FFYzz <cryptochen95@gmail.com>
2021-02-28 21:12:44 +0800

    lesson 27

---
+++ aop-features/src/main/java/cn/ffyzz/spring/aop/features/AspectJAnnotatedPointcutDemo.java
@@ -0,0 +1,32 @@
+package cn.ffyzz.spring.aop.features;
+
+import cn.ffyzz.spring.aop.features.aspect.AspectConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Title:
+ * @Author: FFYzz
+ * @Mail: cryptochen95 at gmail dot com
+ * @Date: 2021/2/28
+ * @Description:
+ */
+@EnableAspectJAutoProxy
+@Configuration
+public class AspectJAnnotatedPointcutDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AspectJAnnotatedPointcutDemo.class, AspectConfiguration.class);
+        context.refresh();
+        AspectJAnnotatedPointcutDemo demo = context.getBean(AspectJAnnotatedPointcutDemo.class);
+        demo.sayHello();
+        context.close();
+    }
+
+    public void sayHello() {
+        System.out.println("hello...");
+    }
+
+}
--- aop-features/src/main/java/cn/ffyzz/spring/aop/features/aspect/AspectConfiguration.java
+++ aop-features/src/main/java/cn/ffyzz/spring/aop/features/aspect/AspectConfiguration.java
@@ -1,6 +1,8 @@
 package cn.ffyzz.spring.aop.features.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * @Title:
@@ -11,5 +13,21 @@ import org.aspectj.lang.annotation.Aspect;
  */
 @Aspect
 public class AspectConfiguration {
+
+    /**
+     * 匹配 join point 的规则定义
+     */
+    @Pointcut("execution(public * *(..))")
+    private void anyPublicMethod() {
+    }
+
+
+    /**
+     * 具体的 join point 拦截动作
+     */
+    @Before("anyPublicMethod()")
+    private void beforeAnyPublicMethod() {
+        System.out.println("@Before any public method..");
+    }
 
 }
